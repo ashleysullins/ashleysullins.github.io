58abd83218c81f022c9c71c29e7ae34ce38ae787
Ashley Sullins <ashley.sullins@gmail.com>
2015-11-30 21:47:33 -0800

    Reorganize js files

--- js/script.js
+++ js/script.js
@@ -6,4 +6,36 @@ $(document).ready(function() {
         $('*[class^="prlx"]').css('top', -(scrolled * 0.5) + 'px');     
       });
   });
+
+  $(".popup").fancybox({
+            maxWidth  : 800,
+            maxHeight : 600,
+            fitToView : true,
+            width   : '70%',
+            height    : '70%',
+            autoSize  : false,
+            closeClick  : false,
+            openEffect  : 'none',
+            closeEffect : 'none'
+          });
+   // Insert Responsive Sidebar Icon
+    $('<div class="responsive-nav-icon" />').appendTo('.section-one');
+    $('<div class="responsive-nav-close" />').appendTo('nav');
+    $('<div id="overlay" />').insertAfter('footer');
+
+    // Navigation Slide In
+    $('.responsive-nav-icon').click(function() {
+    $('nav').addClass('slide-in');
+    $('html').css("overflow", "hidden");
+    $('#overlay').show();
+    return false;
+    });
+
+    // Navigation Slide Out
+    $('#overlay, .responsive-nav-close').click(function() {
+    $('nav').removeClass('slide-in');
+    $('html').css("overflow", "auto");
+    $('#overlay').hide();
+    return false;
+    });
 });
